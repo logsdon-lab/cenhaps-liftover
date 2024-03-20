0274f366dce34a82a8741f93608830a40ff80bb4
koisland <koshima789@gmail.com>
2024-03-20 12:12:32 -0400

    Added liftover of hg19 cenhaps to hg38 first before t2t-chm13. Changed so inputs downloaded.

--- README.md
+++ README.md
@@ -4,7 +4,15 @@ CenHap paper
 * https://elifesciences.org/articles/42989
 
 CenHap coords
-* https://cdn.elifesciences.org/articles/42989/elife-42989-fig2-data1-v1.tds
+* https://elifesciences.org/articles/42989/figures#fig2sdata1
+* Raw file:
+    * https://cdn.elifesciences.org/articles/42989/elife-42989-fig2-data1-v1.tds
 
-Chain file
-* https://genome.ucsc.edu/cgi-bin/hgTrackUi?hgsid=1362452629_UneFYykJjrSS6NfDHXANksNtyvdb&db=hub_3267197_GCA_009914755.4&c=CP068276.2&g=hub_3267197_hgLiftOver
+Chain files
+* hg19 -> hg38
+    * https://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/hg19ToHg38.over.chain.gz
+* hg38 -> t2t-chm13
+    * https://hgdownload.gi.ucsc.edu/hubs/GCA/009/914/755/GCA_009914755.4/liftOver/hg38-chm13v2.over.chain.gz
+* hg19 -> t2t-chm13
+    * Too many unliftable.
+    * https://genome.ucsc.edu/cgi-bin/hgTrackUi?hgsid=1362452629_UneFYykJjrSS6NfDHXANksNtyvdb&db=hub_3267197_GCA_009914755.4&c=CP068276.2&g=hub_3267197_hgLiftOver
--- convert.sh
+++ convert.sh
@@ -2,32 +2,45 @@
 
 set -euo pipefail
 
+wget -O - https://cdn.elifesciences.org/articles/42989/elife-42989-fig2-data1-v1.tds | \
 awk -v FS="\t" \
     -v OFS="\t" \
-    'NR > 1 \
+    'NR > 3 \
     {
         if ($2 != "NA" || $3 != "NA" ) {
-            print $1, $2, $3, "p-arm"
+            print $1, $2, $3, "p-arm" > "hg19_parm_cenhap_coords.bed"
         }
-    }' elife-42989-fig2-data1-v1.tds > hg19_parm_cenhap_coords.bed
-
-awk -v FS="\t" \
-    -v OFS="\t" \
-    'NR > 1 \
-    {
         if ($4 != "NA" || $5 != "NA" ) {
-            print $1, $4, $5, "q-arm"
-        }
-    }' elife-42989-fig2-data1-v1.tds > hg19_qarm_cenhap_coords.bed
+            print $1, $4, $5, "q-arm" > "hg19_qarm_cenhap_coords.bed"
+        } 
+    }'
 
+rm -f hg19ToHg38.over.chain.gz hg38-chm13v2.over.chain.gz
+wget https://hgdownload.cse.ucsc.edu/goldenpath/hg19/liftOver/hg19ToHg38.over.chain.gz
+wget https://hgdownload.gi.ucsc.edu/hubs/GCA/009/914/755/GCA_009914755.4/liftOver/hg38-chm13v2.over.chain.gz
+
+# hg19 -> hg38
 ./liftOver \
     hg19_parm_cenhap_coords.bed \
-    hg19-chm13v2.over.chain.gz \
-    chm13v2_parm_cenhap_coords.bed \
-    parm_cenhaps_unlifted.bed
+    hg19ToHg38.over.chain.gz \
+    hg38_parm_cenhap_coords.bed \
+    hg19ToHg38_parm_cenhaps_unlifted.bed
 
 ./liftOver \
     hg19_qarm_cenhap_coords.bed \
-    hg19-chm13v2.over.chain.gz \
+    hg19ToHg38.over.chain.gz \
+    hg38_qarm_cenhap_coords.bed \
+    hg19ToHg38_qarm_cenhaps_unlifted.bed
+
+# hg38 -> t2t
+./liftOver \
+    hg38_parm_cenhap_coords.bed \
+    hg38-chm13v2.over.chain.gz \
+    chm13v2_parm_cenhap_coords.bed \
+    hg38-chm13v2_parm_cenhaps_unlifted.bed
+
+./liftOver \
+    hg38_qarm_cenhap_coords.bed \
+    hg38-chm13v2.over.chain.gz \
     chm13v2_qarm_cenhap_coords.bed \
-    qarm_cenhaps_unlifted.bed
+    hg38-chm13v2_qarm_cenhaps_unlifted.bed
